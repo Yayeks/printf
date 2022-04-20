5dd484aaebd459f3f2e0d4a09ee92e224df50572
Yayeks <anhandle1@gmail.com>
2022-04-20 00:16:22 -0700

    Header file

--- main.h
+++ main.h
@@ -16,4 +16,13 @@ typedef struct op
 	int (*f)(va_list);
 } op_t;
 
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_list);
+int print_s(va_list ar_list);
+int print_d(va_list ar_list);
+int print_i(va_list ar_numlist);
+int print_b(va_list binary_list);
+
 #endif
